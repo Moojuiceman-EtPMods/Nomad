209b846e0805a3d46ce0bd30325226b510e93ea5
Alex Barrett <moojuiceman@hotmail.co.uk>
2023-01-19 19:37:58 +0000

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,96 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Nomad
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        static ManualLogSource logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            logger = Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded");
+            Logger.LogInfo($"Patching...");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger.LogInfo($"Patched");
+        }
+
+        [HarmonyPatch(typeof(ItemStats), "Awake")]
+        [HarmonyPostfix]
+        static void Awake_Postfix(ItemStats __instance)
+        {
+            // Debug drawing
+            /*
+            if (__instance.GetComponent<AttachSystemRaftBase>() != null)
+            {
+                foreach (var ag in __instance.GetComponent<AttachSystemRaftBase>().attachGroups)
+                {
+                    foreach (var acp in ag.attachCheckPoints)
+                    {
+                        LineRenderer lr = acp.gameObject.AddComponent<LineRenderer>();
+                        lr.useWorldSpace = false;
+                        lr.startWidth = .2f;
+                        lr.endWidth = .05f;
+                        lr.SetPositions(new Vector3[] { new Vector3(0, 0, 0), acp.transform.up.normalized * .1f });
+
+                        var acpgo = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        UnityEngine.Object.Destroy(acpgo.GetComponent<SphereCollider>());
+                        acpgo.transform.SetParent(acp.transform);
+                        //acpgo.transform.localScale = new Vector3(.1f, .1f, .1f);
+                    }
+                }
+            }*/
+
+            if ((__instance.AttachToTerrain && __instance.GetComponent<AttachSystemRaftBase>() == null && __instance.Tag != "SunClock_Simple") || __instance.Tag == "Fish_Trap")
+            {
+                logger.Log(LogLevel.Debug, $"Enabling attach for {__instance.Tag}");
+
+                CraftableObject co = __instance.GetComponent<CraftableObject>();
+                if (co == null)
+                {
+                    __instance.Draggable = true;
+                }
+                else
+                {
+                    AccessTools.Field(typeof(CraftableObject), "origDraggable").SetValue(co, true);
+                }
+
+                GameObject ap = new GameObject();
+                ap.transform.SetParent(__instance.gameObject.transform);
+                ap.transform.localPosition = new Vector3(0, 0, 0);
+                ap.transform.localRotation = Quaternion.identity;
+                ap.transform.up *= -1;
+
+                AttachSystemRaftBase asrb = __instance.gameObject.AddComponent<AttachSystemRaftBase>();
+                asrb.attachGroups = new AttachSystemRaftBase.attachGroup_t[] {
+                    new AttachSystemRaftBase.attachGroup_t() {
+                        attachCheckPoints = new Transform[] {
+                            ap.transform, ap.transform, ap.transform, ap.transform
+                        }
+                    }
+                };
+                asrb.attachingObject = ObjectManager.Instance.GetObject("Rope_Strong");
+
+                // Debug drawing
+                /*
+                LineRenderer lr = ap.AddComponent<LineRenderer>();
+                lr.useWorldSpace = false;
+                lr.startWidth = .2f;
+                lr.endWidth = .05f;
+                lr.SetPositions(new Vector3[] { new Vector3(0, 0, 0), ap.transform.up.normalized * .1f });
+                
+                GameObject g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                UnityEngine.Object.Destroy(g.GetComponent<SphereCollider>());
+                g.transform.SetParent(ap.transform);
+                g.transform.localPosition = new Vector3(0, 0, 0);
+                g.transform.localScale = new Vector3(.1f, .1f, .1f);
+                */
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Nomad
+
+This is a mod for the game Escape The Pacific by Gamers4Gamers Team. You can get it here: https://store.steampowered.com/app/655290/Escape_The_Pacific/
+
+#### What it does
+
+Lets you move and attach extra things to rafts.
+
+* Fish traps
+* Anything that snaps to terrain level (except sundials). Composters, big planters, water collectors etc.
+
+## !! Warning !!
+This mod **will** affect your save file, due to the way rafts are saved. Saving a raft with items attached by this mod and loading the save without the mod, **will** cause weird behaviour and break things.
+
+* You should backup your saves before using this mod.
+* Before playing without it you should detach the not-usually-attachable items and save again.
+
+#### Save file backup
+
+* Saves are located at C:\Users\\\<Username>\AppData\LocalLow\Gamers4Gamers Team\Escape The Pacific\SavesDir
+* Make a copy of the folder, either to another location or as a new folder name in the same place
+* If sharing the save, zip the folder for easier transfer
+
+#### Installation
+
+* Download BepInEx 5 64-bit. Here is a direct link to version 5.4.21 which works with the game https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x64_5.4.21.0.zip
+* Unzip it into the game folder, next to EscapeThePacific.exe. Find it via Steam by right clicking the game > Manage > Browse local files
+* Start the game once so BepInEx creates the necessary folders
+* Grab the latest .dll from the releases page and put it in the BepInEx\plugins folder
+ 
+#### Configuration
+
+Mod options are stored in BepInEx\config\\\<Modname>.cfg
+
+This mod has no configurable options.
